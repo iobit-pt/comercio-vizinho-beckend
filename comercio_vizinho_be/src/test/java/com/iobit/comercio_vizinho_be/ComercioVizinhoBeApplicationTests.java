07cb6dfc1364cf13c9b791f5c0bfdbbf1d5ee72a
Wes <wal2.designer@gmail.com>
2024-08-05 12:10:58 +0100

    Adding springboot

---
+++ comercio_vizinho_be/src/test/java/com/iobit/comercio_vizinho_be/ComercioVizinhoBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iobit.comercio_vizinho_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComercioVizinhoBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
