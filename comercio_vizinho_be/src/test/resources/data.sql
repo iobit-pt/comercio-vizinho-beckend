abb0a94d6e4415c7125bfd7eab180591912b6545
Wes Benvindo <wal2.designer@gmail.com>
2024-09-25 11:16:19 +0100

    Adding some entry to sql data

--- comercio_vizinho_be/src/test/resources/data.sql
+++ comercio_vizinho_be/src/test/resources/data.sql
@@ -12,3 +12,11 @@ INSERT INTO tb_adresses (ID, STREET, NUMBER, DETAIL, POSTAL_CODE)
 VALUES (100, 'Rua das amoras', '123', 'Casa de tijolo', '2222-123');
 INSERT INTO tb_adresses (ID, STREET, NUMBER, DETAIL, POSTAL_CODE)
 VALUES (101, 'Rua dos porquinhos', '1', 'casa de palha, ao lado da cada de madeira', 'PIG-035-000');
+
+-- GOODS
+INSERT INTO tb_goods (ID, NAME, DESCRIPTION, PRICE)
+VALUES (99, 'Pêssego dos grandes', 'Nacional mesmo', 1.85) INSERT
+INSERT INTO tb_goods (ID, NAME, DESCRIPTION, PRICE)
+VALUES (100, 'Uva roxa', 'Uva para fazer vinho tinto', 2.85);
+INSERT INTO tb_goods (ID, NAME, DESCRIPTION, PRICE)
+VALUES (101, 'Pera rocha', 'Rocha acha', 1.33);
