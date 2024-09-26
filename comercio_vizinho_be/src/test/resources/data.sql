30bfc4ec6fa227bb865b76e5953dac51a930750a
Wes Benvindo <wal2.designer@gmail.com>
2024-09-26 14:12:13 +0100

    wip: adding some services to data

--- comercio_vizinho_be/src/test/resources/data.sql
+++ comercio_vizinho_be/src/test/resources/data.sql
@@ -13,7 +13,12 @@ VALUES (100, 'Rua das amoras', '123', 'Casa de tijolo', '2222-123');
 INSERT INTO tb_adresses (ID, STREET, NUMBER, DETAIL, POSTAL_CODE)
 VALUES (101, 'Rua dos porquinhos', '1', 'casa de palha, ao lado da cada de madeira', 'PIG-035-000');
 
--- GOODS
+-- GOODS PRODUCTS
 INSERT INTO tb_goods (ID, NAME, DESCRIPTION, PRICE, USER_ID, GOODS_TYPE) VALUES (99, 'Pêssego dos grandes', 'Nacional mesmo', 1.85, 99, 1);
 INSERT INTO tb_goods (ID, NAME, DESCRIPTION, PRICE, USER_ID, GOODS_TYPE) VALUES (100, 'Uva roxa', 'Uva para fazer vinho tinto', 2.85, 99, 1);
 INSERT INTO tb_goods (ID, NAME, DESCRIPTION, PRICE, USER_ID, GOODS_TYPE) VALUES (101, 'Pera rocha', 'Rocha acha', 1.33, 99, 1);
+
+-- GOODS SERVICES
+INSERT INTO tb_goods (ID, NAME, DESCRIPTION, PRICE, USER_ID, GOODS_TYPE) VALUES (103, 'Cortes a laser', 'corto qualquer coisa', 1.85, 99, 2);
+INSERT INTO tb_goods (ID, NAME, DESCRIPTION, PRICE, USER_ID, GOODS_TYPE) VALUES (104, 'Lava carro', 'Lavo seu carro', 2.85, 99, 2);
+INSERT INTO tb_goods (ID, NAME, DESCRIPTION, PRICE, USER_ID, GOODS_TYPE) VALUES (105, 'Massagem', 'Faço massagens', 1.33, 99, 2);
